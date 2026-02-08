fc3266a2205950bec3662fe509c105a318eb8af9
ryankhann <ryankhann028@gmail.com>
2026-02-08 18:38:13 +0600

    Fix Aiven MySQL SSL using CA certificate

--- db.js
+++ db.js
@@ -1,15 +1,20 @@
-// db.js
-import mysql from 'mysql2/promise';
+import mysql from "mysql2/promise";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-export const db = mysql.createPool({
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
   ssl: {
-    rejectUnauthorized: true
+    ca: fs.readFileSync(path.join(__dirname, "certs/aiven-ca.pem")),
   },
-  waitForConnections: true,
-  connectionLimit: 10
 });
+
+await pool.query("SELECT 1");
