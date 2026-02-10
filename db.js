0c45d951236ba13fbf7c0eded47be4a9176a6214
ryankhann <ryankhann028@gmail.com>
2026-02-10 11:15:44 +0600

    Fix MySQL SSL by loading Aiven CA cert from env variable

--- db.js
+++ db.js
@@ -1,18 +1,17 @@
 // db.js
 import mysql from 'mysql2';
-import fs from 'fs';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 export const db = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: {
-    ca: fs.readFileSync('./certs/ca.pem')
+    ca: process.env.DB_SSL_CA
   },
   waitForConnections: true,
   connectionLimit: 10,
